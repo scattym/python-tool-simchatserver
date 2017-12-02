39c77d3ff4c156151f48bc767f97fa66952a4157
mattjclark0407@hotmail.com <mattjclark0407@hotmail.com>
2017-12-02 12:02:29 +1100

    Rename all references to simchatserver.

--- buildandrun.sh
+++ buildandrun.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
 version=`git branch | grep '*' | grep -v master | cut -f 2 -d '-'`
-running_version=`docker ps | grep chatserver | awk '{print $NF}' | cut -f 2 -d '-'`
+running_version=`docker ps | grep simchatserver | awk '{print $NF}' | cut -f 2 -d '-'`
 if [ "X${version}" != "X" ] ; then
 	if [ "X${version}" != "X${running_version}" ] ; then
-		docker build --tag=chatserver:v${version} . -f dockerfiles/chatserver/Dockerfile
+		docker build --tag=simchatserver:v${version} . -f dockerfiles/simchatserver/Dockerfile
 		if [ $? -eq 0 ] ; then
 			if [ "X${running_version}" != "X" ] ; then
-				docker stop chatserver-${running_version} 
+				docker stop simchatserver-${running_version}
 			fi
 			if [ $? -eq 0 ] ; then
-				docker run -p 0.0.0.0:65533:65533 -dt --name=chatserver-${version} --restart=always chatserver:v${version} -v
+				docker run -p 0.0.0.0:65533:65533 -dt --name=simchatserver-${version} --restart=always simchatserver:v${version} -v
 			fi
 		fi
 		
 	else
-		echo "chatserver: Running version is already ${version}"
+		echo "simchatserver: Running version is already ${version}"
 	fi
 else
-	echo "chatserver: No version information for chatserver"
+	echo "simchatserver: No version information for simchatserver"
 fi
 
 
--- keepupdated.sh
+++ keepupdated.sh
@@ -2,11 +2,11 @@
 
 while true; do
     if git pull | grep -q "Already up-to-date" ; then
-        echo "`date`: Already up to date"
+        echo "`date`: simchatserver already up to date"
     else
-        docker build . --tag=nmeaproxy:dev -f Dockerfile-dev && \
-        docker stop nmeaproxy && \
-        docker rm nmeaproxy && docker run -dt --restart=always --name=nmeaproxy -e GEO_API_HOST=localhost:8001 -p 65535:5000 nmeaproxy:dev -x -s geotool.scattym.com:8001 -t -x
+        docker build . --tag=simchatserver:dev -f dockerfiles/simchatserver/Dockerfile && \
+        docker stop simchatserver && \
+        docker rm simchatserver && docker run -dt --restart=always --name=simchatserver -v
     fi
     echo "`date`: Sleeping for 60 seconds"
     sleep 60
