2d381880bee768af8b1136831eb89b991fb7f6e0
Matt Clark <mattjclark0407@hotmail.com>
2017-12-02 17:13:03 +1100

    Fixup reference to docker image

--- keepupdated.sh
+++ keepupdated.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 while true; do
-    if git pull | grep -q "Already up-to-date" ; then
+    if git pull | grep -q "simchatserver already up-to-date" ; then
         echo "`date`: simchatserver already up to date"
     else
         docker build . --tag=simchatserver:dev -f dockerfiles/simchatserver/Dockerfile && \
         docker stop simchatserver && \
-        docker rm simchatserver && docker run -dt --restart=always --name=simchatserver -v
+        docker rm simchatserver && docker run -dt --restart=always --name=simchatserver simchatserver:dev -v
     fi
     echo "`date`: Sleeping for 60 seconds"
     sleep 60
