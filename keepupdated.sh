dff1d4bc1489bf14c4d4de1c35d4f6f9e9011fe2
Matt Clark <mattjclark0407@hotmail.com>
2017-12-02 17:15:04 +1100

    Clean up old docker images on restart

--- keepupdated.sh
+++ keepupdated.sh
@@ -7,6 +7,7 @@ while true; do
         docker build . --tag=simchatserver:dev -f dockerfiles/simchatserver/Dockerfile && \
         docker stop simchatserver && \
         docker rm simchatserver && docker run -dt --restart=always --name=simchatserver -p 65533:65533 simchatserver:dev -v
+        docker images | grep none | awk '{print $3}' | xargs docker rmi
     fi
     echo "`date`: Sleeping for 60 seconds"
     sleep 60
