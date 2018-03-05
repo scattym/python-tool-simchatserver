81139dfaefacc486b4eeaf85118410a4fc1a3605
mattjclark0407@hotmail.com <mattjclark0407@hotmail.com>
2018-03-05 23:16:32 +1100

    Fix up variable names overlapping with system values.

--- sim_chat_lib/simcom53XX/encapsulation.py
+++ sim_chat_lib/simcom53XX/encapsulation.py
@@ -4,20 +4,21 @@ import logging
 
 logger = logging.getLogger(__name__)
 
-def command_as_json(type, cmd, key, seed=None):
+
+def command_as_json(cmd_type, cmd, hash_key, seed=None):
     config = {
-        type: str(cmd.rstrip()),
+        cmd_type: str(cmd.rstrip()),
     }
 
     sha256 = hashlib.sha256()
-    sha256.update(key.encode())
+    sha256.update(hash_key.encode())
     if seed is not None:
         sha256.update(seed)
     for key in sorted(config):
         logger.debug("Key: %s, value: %s", key, config[key])
         sha256.update(key.encode())
         sha256.update(("%s" % (config[key])).encode())
-    sha256.update(key.encode())
+    sha256.update(hash_key.encode())
     config["checksum"] = sha256.hexdigest()
 
     return json.dumps(config)
