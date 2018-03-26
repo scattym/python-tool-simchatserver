24bea5f0aaf2cbd7f3a9e7484df98089b1a63748
mattjclark0407@hotmail.com <mattjclark0407@hotmail.com>
2018-03-26 22:33:18 +1100

    Change keepupdated to use latest image instead of dev.

--- keepupdated.sh
+++ keepupdated.sh
@@ -4,9 +4,9 @@ while true; do
     if git pull | grep -q "Already up-to-date" ; then
         echo "`date`: simchatserver already up to date"
     else
-        docker build . --tag=simchatserver:dev -f dockerfiles/simchatserver/Dockerfile && \
+        docker build . --tag=simchatserver -f dockerfiles/simchatserver/Dockerfile && \
         docker stop simchatserver && \
-        docker rm simchatserver && docker run -dt --restart=always --name=simchatserver -p 65533:65533 simchatserver:dev -v
+        docker rm simchatserver && docker run -dt --restart=always --name=simchatserver -p 65533:65533 simchatserver -v
         docker images | grep none | awk '{print $3}' | xargs docker rmi
     fi
     echo "`date`: Sleeping for 60 seconds"
