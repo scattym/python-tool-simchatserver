777ce76229b1c852f27cbbc162e2b3974a6936ae
mattjclark0407@hotmail.com <mattjclark0407@hotmail.com>
2018-03-29 15:52:03 +1100

    Update api host as not available on localhost on dev server.

--- keepupdated.sh
+++ keepupdated.sh
@@ -6,7 +6,7 @@ while true; do
     else
         docker build . --tag=simchatserver -f dockerfiles/simchatserver/Dockerfile && \
         docker stop simchatserver && \
-        docker rm simchatserver && docker run -dt --restart=always --name=simchatserver -p 65533:65533 simchatserver -v
+        docker rm simchatserver && docker run -dt --restart=always --name=simchatserver -p 65533:65533 simchatserver -v -s 10.1.1.4:8000
         docker images | grep none | awk '{print $3}' | xargs docker rmi
     fi
     echo "`date`: Sleeping for 60 seconds"
