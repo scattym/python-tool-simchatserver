0d7769c2c3585a7df26917eea53764a49466f030
mjclark1 <mattjclark0407@hotmail.com>
2018-05-12 18:08:50 +1000

    Move cell updates to coroutines. Made api a little more readable.

--- sim_chat_lib/geotool_db_api/common.py
+++ sim_chat_lib/geotool_db_api/common.py
@@ -30,29 +30,39 @@ async def create_pool_a(*args, **kwargs):
         # CONXN_POOL = ThreadedConnectionPool(1, 10, *args, **kwargs)
 
 
-def execute_sql_update_a(query, *args):
+async def execute_sql_update_a(query, *args):
     # loop = asyncio.get_event_loop()
-    print("Executing update sql")
-    print(*args)
-    rows = execute_sql_a(query, *args)
-
-    return rows
+    # print("Executing update sql")
+    # print(*args)
+    return await execute_sql_a(query, *args)
 
 
-async def execute_sql_a(query, *args):
-    print(*args)
+async def execute_sql_fetchval_a(query, *args):
+    # print(*args)
     global CONXN_POOL
     if not CONXN_POOL:
         await create_pool_a(**get_db_from_env())
+
     async with CONXN_POOL.acquire() as connection:
         async with connection.transaction():
             result = await connection.fetchval(query, *args)
             return result
 
 
+async def execute_sql_a(query, *args):
+    # print(*args)
+    global CONXN_POOL
+    if not CONXN_POOL:
+        await create_pool_a(**get_db_from_env())
+
+    async with CONXN_POOL.acquire() as connection:
+        async with connection.transaction():
+            await connection.execute(query, *args)
+
+
 def execute_sql_get_a(query, *args):
     # loop = asyncio.get_event_loop()
 
-    rows = execute_sql_a(query, *args)
+    rows = execute_sql_fetchval_a(query, *args)
 
     return rows
--- sim_chat_lib/geotool_db_api/device_api.py
+++ sim_chat_lib/geotool_db_api/device_api.py
@@ -5,6 +5,17 @@ from sim_chat_lib.geotool_db_api import common
 
 GET_DEVICE_ID_SQL = """SELECT id FROM device_device WHERE imei=$1"""
 
+INSERT_CELLUPDATE_SQL = """
+    INSERT INTO
+        device_celldata (
+            log_time, cell_id, location_area_code, mobile_country_code, mobile_network_code, primary_scrambling_code,
+            secondary_synchronisation_code, rx_level, lookup_method, device_id
+        )
+    VALUES (
+        $1, $2, $3, $4, $5, $6, $7, $8, $9, $10
+    )
+"""
+
 INSERT_DEVICEUPDATE_SQL = """ 
  INSERT INTO
    device_deviceupdate (
@@ -15,6 +26,7 @@ INSERT_DEVICEUPDATE_SQL = """
     $1, $2, ST_SetSRID(ST_MakePoint($3, $4), 4326), $5, $6, $7, $8, $9, $10, $11, $12
   )
 """
+
 UPDATE_DEVICE_CACHE_SQL = """ 
  UPDATE
   device_deviceupdatecache
@@ -32,38 +44,52 @@ UPDATE_DEVICE_CACHE_SQL = """
 """
 
 
-def get_device_id(imei):
-    rows = common.execute_sql_get_a(GET_DEVICE_ID_SQL, imei)
+async def get_device_id(imei):
+    rows = await common.execute_sql_get_a(GET_DEVICE_ID_SQL, imei)
     return rows
 
 
-def insert_deviceupdate(
-        device_id:int, imei:str, latitude:float, longitude:float, true_track:int, mag_track:int, ground_speed:int,
-        altitude:float, timestamp:datetime.datetime,
-        dilution:float, num_sats:int, log_time:datetime.datetime
+def get_insert_cellupdate_coroutine(
+        log_time: datetime.datetime, cell_id: int, location_area_code: int, mobile_country_code: int,
+        mobile_network_code: int,
+        primary_scrambling_code: int, secondary_sychronisation_code: int,
+        rx_level: str,
+        lookup_method: str,
+        device_id: int,
 ):
-    result = common.execute_sql_update_a(
+    return common.execute_sql_a(
+        INSERT_CELLUPDATE_SQL,
+        log_time, cell_id, location_area_code, mobile_country_code, mobile_network_code,
+        primary_scrambling_code, secondary_sychronisation_code, rx_level, lookup_method,
+        device_id
+    )
+
+
+def get_insert_deviceupdate_coroutine(
+        device_id: int, imei: str, latitude: float, longitude: float, true_track: int, mag_track: int,
+        ground_speed: int, altitude: float, timestamp: datetime.datetime,
+        dilution: float, num_sats: int, log_time: datetime.datetime
+):
+    return common.execute_sql_a(
         INSERT_DEVICEUPDATE_SQL,
         device_id, imei, latitude, longitude, true_track,
         mag_track, ground_speed, altitude, timestamp, dilution,
         num_sats, log_time,
 
     )
-    return result
 
 
-def update_deviceupdatecache(
-        latitude:float, longitude:float, true_track:float, mag_track:int, ground_speed:int, altitude:float,
-        timestamp:datetime.datetime, dilution:float, num_sats:int, device_id:int
+def get_update_deviceupdatecache_coroutine(
+        latitude: float, longitude: float, true_track: float, mag_track: int, ground_speed: int, altitude: float,
+        timestamp: datetime.datetime, dilution: float, num_sats: int, device_id: int
 ):
-    result = common.execute_sql_update_a(
+    return common.execute_sql_a(
         UPDATE_DEVICE_CACHE_SQL,
         latitude, longitude, true_track,
         mag_track, ground_speed, altitude, timestamp, dilution,
         num_sats, device_id
 
     )
-    return result
 
 
 if __name__ == '__main__':
@@ -81,6 +107,6 @@ if __name__ == '__main__':
     import datetime
 
     print(get_device_id("33330407"))
-    print(insert_deviceupdate(25, "0407", 0, 0, 0, 0, 0, 0, datetime.datetime.now(), 1, 9, datetime.datetime.now()))
-    print(update_deviceupdatecache(0, 0, 0, 0, 0, 0, datetime.datetime.now(), 1, 9, 25))
+    print(get_insert_deviceupdate_coroutine(25, "0407", 0, 0, 0, 0, 0, 0, datetime.datetime.now(), 1, 9, datetime.datetime.now()))
+    print(get_update_deviceupdatecache_coroutine(0, 0, 0, 0, 0, 0, datetime.datetime.now(), 1, 9, 25))
 
