1f05448f088f176f0bc6da00df2bc79604a57bf4
mjclark1 <mattjclark0407@hotmail.com>
2018-05-19 20:49:51 +1000

    Update description

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
       name='simchatserver',
       version='2.29.dev0',
-      description=('Project to communicate with simcom devices'),
+      description=('Project to communicate with simcom and meitrack devices'),
       url='https://bitbucket.org/mjclark1/python-tool-simchatserver.git',
       author='Matt Clark',
       author_email='mattjclark0407@hotmail.com',
