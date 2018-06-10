53629e49380a6605368e127eaaea6fd3fff80eb5
Matt Clark <mattjclark0407@hotmail.com>
2018-06-10 23:29:21 +1000

    Add dev dockerfiles for simchatserver master builds.

--- buildimages-dev.sh
+++ buildimages-dev.sh
@@ -1,4 +1,4 @@
-docker build --tag=simchatserver-base:interim . -f dockerfiles/simchatserver-base/Dockerfile
+docker build --tag=simchatserver-base-dev:interim . -f dockerfiles/simchatserver-base-dev/Dockerfile
 docker build --tag=gcr.io/geotool-test/simchatserver:v${GIT_VER} . -f dockerfiles/simchatserver/Dockerfile
 docker build --tag=gcr.io/geotool-test/mqrecv-cell-update:v${GIT_VER} . -f dockerfiles/mqrecv-cell-update/Dockerfile
 docker build --tag=gcr.io/geotool-test/mqrecv-event:v${GIT_VER} . -f dockerfiles/mqrecv-event/Dockerfile
