bb402cd734e7330fa06a5da3087c382c81b99b50
mjclark1 <mattjclark0407@hotmail.com>
2018-07-07 15:07:56 +1000

    Update buildimages-dev to use all downstream *-dev docker files.

--- buildimages-dev.sh
+++ buildimages-dev.sh
@@ -1,8 +1,8 @@
 docker build --tag=simchatserver-base-dev:interim . -f dockerfiles/simchatserver-base-dev/Dockerfile
-docker build --tag=gcr.io/geotool-test/simchatserver:v${GIT_VER} . -f dockerfiles/simchatserver/Dockerfile
+docker build --tag=gcr.io/geotool-test/simchatserver:v${GIT_VER} . -f dockerfiles/simchatserver/Dockerfile-dev
 docker build --tag=gcr.io/geotool-test/mqrecv-cell-update:v${GIT_VER} . -f dockerfiles/mqrecv-cell-update/Dockerfile
-docker build --tag=gcr.io/geotool-test/mqrecv-event:v${GIT_VER} . -f dockerfiles/mqrecv-event/Dockerfile
-docker build --tag=gcr.io/geotool-test/mqrecv-gps-update:v${GIT_VER} . -f dockerfiles/mqrecv-gps-update/Dockerfile
-docker build --tag=gcr.io/geotool-test/mqrecv-firmware-update:v${GIT_VER} . -f dockerfiles/mqrecv-firmware-update/Dockerfile
-docker build --tag=gcr.io/geotool-test/mqrecv-mt-file:v${GIT_VER} . -f dockerfiles/mqrecv-mt-file/Dockerfile
-docker build --tag=gcr.io/geotool-test/simchatcelery:v${GIT_VER} . -f dockerfiles/simchatcelery/Dockerfile
+docker build --tag=gcr.io/geotool-test/mqrecv-event:v${GIT_VER} . -f dockerfiles/mqrecv-event/Dockerfile-dev
+docker build --tag=gcr.io/geotool-test/mqrecv-gps-update:v${GIT_VER} . -f dockerfiles/mqrecv-gps-update/Dockerfile-dev
+docker build --tag=gcr.io/geotool-test/mqrecv-firmware-update:v${GIT_VER} . -f dockerfiles/mqrecv-firmware-update/Dockerfile-dev
+docker build --tag=gcr.io/geotool-test/mqrecv-mt-file:v${GIT_VER} . -f dockerfiles/mqrecv-mt-file/Dockerfile-dev
+docker build --tag=gcr.io/geotool-test/simchatcelery:v${GIT_VER} . -f dockerfiles/simchatcelery/Dockerfile-dev
