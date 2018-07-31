24a0875203dad945f347a95c0aec7e93d9a50b47
Matt Clark <mattjclark0407@hotmail.com>
2018-07-31 11:46:24 +1000

    Handle key and seed potentially being byte object already.

--- sim_chat_lib/simcom53XX/encapsulation.py
+++ sim_chat_lib/simcom53XX/encapsulation.py
@@ -25,14 +25,22 @@ def command_as_json(cmd_type, cmd, hash_key, seed=None):
 
 
 def calc_hash(key, seed, *args):
+    try:
+        key = key.encode()
+    except AttributeError as _:
+        pass
+    try:
+        seed = seed.encode()
+    except AttributeError as _:
+        pass
     sha256 = hashlib.sha256()
-    sha256.update(key.encode())
+    sha256.update(key)
     if seed is not None:
-        sha256.update(seed.encode())
+        sha256.update(seed)
     for value in args:
         logger.log(13, "Value: %s", value)
         sha256.update(("%s" % (value,)).encode())
-    sha256.update(key.encode())
+    sha256.update(key)
 
     return sha256.hexdigest()
 
