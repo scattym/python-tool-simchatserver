7289d819f355384bf6bfe3f86b024573a36810f5
Matt Clark <mattjclark0407@hotmail.com>
2018-08-08 22:30:26 +1000

    Add meitrackbot to build list.

--- buildandpush.sh
+++ buildandpush.sh
@@ -11,6 +11,7 @@ if [ "X${version}" != "X" ] ; then
     docker build --tag=gcr.io/geotool-test/mqrecv-gps-update:v${version} . -f dockerfiles/mqrecv-gps-update/Dockerfile
     docker build --tag=gcr.io/geotool-test/mqrecv-firmware-update:v${version} . -f dockerfiles/mqrecv-firmware-update/Dockerfile
     docker build --tag=gcr.io/geotool-test/mqrecv-mt-file:v${version} . -f dockerfiles/mqrecv-mt-file/Dockerfile
+    docker build --tag=gcr.io/geotool-test/meitrackbot:v${version} . -f dockerfiles/meitrackbot/Dockerfile
 
     if [ $? -eq 0 ] ; then
         gcloud docker -- push gcr.io/geotool-test/simchatserver:v${version}
@@ -19,18 +20,22 @@ if [ "X${version}" != "X" ] ; then
         gcloud docker -- push gcr.io/geotool-test/mqrecv-gps-update:v${version}
         gcloud docker -- push gcr.io/geotool-test/mqrecv-firmware-update:v${version}
         gcloud docker -- push gcr.io/geotool-test/mqrecv-mt-file:v${version}
+        gcloud docker -- push gcr.io/geotool-test/meitrackbot:v${version}
         docker tag gcr.io/geotool-test/simchatserver:v${version} gcr.io/position-live/simchatserver:v${version}
         docker tag gcr.io/geotool-test/mqrecv-cell-update:v${version} gcr.io/position-live/mqrecv-cell-update:v${version}
         docker tag gcr.io/geotool-test/mqrecv-event:v${version} gcr.io/position-live/mqrecv-event:v${version}
         docker tag gcr.io/geotool-test/mqrecv-gps-update:v${version} gcr.io/position-live/mqrecv-gps-update:v${version}
         docker tag gcr.io/geotool-test/mqrecv-firmware-update:v${version} gcr.io/position-live/mqrecv-firmware-update:v${version}
         docker tag gcr.io/geotool-test/mqrecv-mt-file:v${version} gcr.io/position-live/mqrecv-mt-file:v${version}
+        docker tag gcr.io/geotool-test/meitrackbot:v${version} gcr.io/position-live/meitrackbot:v${version}
+
         gcloud docker -- push gcr.io/position-live/simchatserver:v${version}
         gcloud docker -- push gcr.io/position-live/mqrecv-cell-update:v${version}
         gcloud docker -- push gcr.io/position-live/mqrecv-event:v${version}
         gcloud docker -- push gcr.io/position-live/mqrecv-gps-update:v${version}
         gcloud docker -- push gcr.io/position-live/mqrecv-firmware-update:v${version}
         gcloud docker -- push gcr.io/position-live/mqrecv-mt-file:v${version}
+        gcloud docker -- push gcr.io/position-live/meitrackbot:v${version}
 
     fi
 else
