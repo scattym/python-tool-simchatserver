904a533fb43d5cf96310cec5f1495fbd707d2db5
mjclark1 <mattjclark0407@hotmail.com>
2018-09-16 19:38:38 +1000

    Back to development: 2.80

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
       name='simchatserver',
-      version='2.79',
+      version='2.80.dev0',
       description='Project to communicate with simcom and meitrack devices',
       url='https://bitbucket.org/mjclark1/python-tool-simchatserver.git',
       author='Matt Clark',
