ac1d09be994b78009bf1e6c0572e47f27d997c02
mjclark1 <mattjclark0407@hotmail.com>
2019-01-28 15:26:19 +1100

    Remove port 5555 from the celery docker container.

--- keepupdated.sh
+++ keepupdated.sh
@@ -19,7 +19,7 @@ docker stop simchatcelery || true
 docker rm simchatcelery || true
 docker run -dt --link=simchatserver --restart=always -e SCS_HOST=simchatserver -e CELERY_BROKER_URL="amqp://10.1.1.4" -e GEO_API_HOST=10.1.1.4:8000 \
     -e MQ_HOST=10.1.1.4 -e DBHOST=10.1.1.4 -e DB_POOL_MAX=10 -e DB_POOL_MIN=1 -e FLOWER_BROKER="amqp://guest:guest@10.1.1.4:5672//" \
-    -e FLOWER_BROKER_API="http://guest:guest@10.1.1.4:15672/api/" -p 5555:5555 --name=simchatcelery gcr.io/geotool-test/simchatcelery:v${GIT_VER}
+    -e FLOWER_BROKER_API="http://guest:guest@10.1.1.4:15672/api/" --name=simchatcelery gcr.io/geotool-test/simchatcelery:v${GIT_VER}
 
 docker stop simchatflower || true
 docker rm simchatflower || true
