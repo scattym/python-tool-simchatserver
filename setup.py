24400fc0b20cd95d036505997dbecacfbf8363c3
mjclark1 <mattjclark0407@hotmail.com>
2019-07-02 19:14:26 +1000

    Bump meitrack to 2.10 as 2.9 failed to release.

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
             'aio_pika',
             'geotooldbapi>=2.5',
             'geotoolapi>=2.5',
-            'meitrackparser>=2.9',
+            'meitrackparser>=2.10',
             'celery[sqs]',
             'kombu',
             'tornado==5.1.1',
